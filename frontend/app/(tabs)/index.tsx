fe35201fad2ca378eb8aa94becb2864df2620e2b
AlexZhonn <zhuen.zhong@outlook.com>
2026-01-21 21:08:20 -0500

    feat(frontend): initialize frontend, add sections for data showing

---
+++ frontend/app/(tabs)/index.tsx
@@ -0,0 +1,62 @@
+import { Image } from "expo-image";
+import { Platform, StyleSheet } from "react-native";
+
+import { HelloWave } from "@/components/hello-wave";
+import ParallaxScrollView from "@/components/parallax-scroll-view";
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+import { Link } from "expo-router";
+import StatsGrid from "@/components/ui/stats-grid";
+
+export default function () {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
+      headerImage={
+        <Image
+          source={require("@/assets/images/partial-react-logo.png")}
+          style={styles.reactLogo}
+        />
+      }
+    >
+      <ThemedView
+        style={{
+          flex: 1,
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <StatsGrid
+          backgroundColor={"#00004d"}
+          borderColor={"blue"}
+          title="Finance"
+        />
+        <StatsGrid
+          backgroundColor={"#008000"}
+          borderColor={"green"}
+          title="Nutrition"
+        />
+      </ThemedView>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+  },
+});
